4a2ad3faaf422cd4d52161506ca75089db345d9a
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-12 20:59:57 +0900

    Member 엔티티 개발, 연관관계 설정, 연관관계 편의 메서드 생성

--- src/main/java/springJpaBoard/Board/domain/Board.java
+++ src/main/java/springJpaBoard/Board/domain/Board.java
@@ -3,10 +3,10 @@ package springJpaBoard.Board.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter //추후에 Getter 모두 없앨 예정
@@ -14,7 +14,12 @@ import java.time.LocalDateTime;
 public class Board {
     @Id
     @GeneratedValue
+    @Column(name = "board_id")
     private int id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id") //FK
+    private Member member; //연관관계의 주인
 
     private String title;
 
@@ -23,5 +28,13 @@ public class Board {
     private String writer;
 
     private LocalDateTime boardDateTime;
+
+    /*
+    연관관계 편의 메서드 - 위치는 핵심적으로 컨트롤하는 곳에 작성
+     */
+    public void setMember(Member member) {
+        this.member = member;
+        member.getBoardList().add(this);
+    }
 
 }
--- src/main/java/springJpaBoard/Board/domain/Member.java
+++ src/main/java/springJpaBoard/Board/domain/Member.java
@@ -1,4 +1,28 @@
 package springJpaBoard.Board.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Member {
+
+    @GeneratedValue
+    @Id
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String gender;
+
+    @OneToMany(mappedBy = "member")
+    private List<Board> boardList = new ArrayList<>();
+
+
 }
