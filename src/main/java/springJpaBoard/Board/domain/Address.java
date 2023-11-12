27b47746c0d6158df9c0aaa5d135e89a19a09a41
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-12 22:30:02 +0900

    Address Value Type 추가

---
+++ src/main/java/springJpaBoard/Board/domain/Address.java
@@ -0,0 +1,26 @@
+package springJpaBoard.Board.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    /*
+     jpa 기본 스펙이 생성을 할 때 reflection, proxy 등의 기술을 써야하기 때문에 기본 생성자가 필요함.
+     */
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
