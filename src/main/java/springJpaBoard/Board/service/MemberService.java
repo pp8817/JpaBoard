01f2d08fd7b20008134e4295638364019dc20439
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-13 22:09:41 +0900

    MemberService에 중복 회원 검증 추가

--- src/main/java/springJpaBoard/Board/service/MemberService.java
+++ src/main/java/springJpaBoard/Board/service/MemberService.java
@@ -21,8 +21,20 @@ public class MemberService {
      */
     @Transactional(readOnly = false)
     public Long join(Member member) {
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
+    }
+
+    /**
+     * 중복 회원 검사
+     */
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 
     /**
