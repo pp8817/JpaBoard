eb5e16785b1691d02a66a119ccd9711a97c80ff5
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-12-17 20:51:22 +0900

    세션 정보로 불러온 멤버 객체를 DTO로 변경해서 반환

--- src/main/java/springJpaBoard/Board/controller/HomeController.java
+++ src/main/java/springJpaBoard/Board/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import springJpaBoard.Board.SesstionConst;
+import springJpaBoard.Board.controller.responsedto.MemberResponseDto;
 import springJpaBoard.Board.domain.Member;
 import springJpaBoard.Board.repository.MemberRepository;
 
@@ -26,7 +27,9 @@ public class HomeController {
 
         /*DTO로 감싸기!*/
         //세션이 유지되면 로그인 홈으로 이동
-        model.addAttribute("member", loginMember);
+        MemberResponseDto memberResponseDto = new MemberResponseDto(loginMember);
+
+        model.addAttribute("member", memberResponseDto);
         return "loginHome";
     }
 }
