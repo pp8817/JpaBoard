dedefe5455d8190014a1137cc6094d5c1213cf9e
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-01-07 17:06:05 +0900

    [fix] 회원 성별 필드인 Gender 타입이 Enum인 경우 회원 검색에서 오류 발생 String으로 변경

--- src/main/java/springJpaBoard/Board/repository/search/BoardSearch.java
+++ src/main/java/springJpaBoard/Board/repository/search/BoardSearch.java
@@ -2,13 +2,12 @@ package springJpaBoard.Board.repository.search;
 
 import lombok.Getter;
 import lombok.Setter;
-import springJpaBoard.Board.domain.status.GenderStatus;
 
 @Setter
 @Getter
 public class BoardSearch {
 
-    private GenderStatus memberGender; //회원 성별
+    private String memberGender; //회원 성별
     private String boardTitle; //게시판 이름
 
     public boolean searchIsEmpty() {
--- src/main/java/springJpaBoard/Board/repository/search/MemberSearch.java
+++ src/main/java/springJpaBoard/Board/repository/search/MemberSearch.java
@@ -2,15 +2,12 @@ package springJpaBoard.Board.repository.search;
 
 import lombok.Getter;
 import lombok.Setter;
-import springJpaBoard.Board.domain.status.GenderStatus;
-
-import static springJpaBoard.Board.domain.status.GenderStatus.*;
 
 @Setter
 @Getter
 public class MemberSearch {
     private String memberName;
-    private GenderStatus memberGender = EX; //성별 [남성, 여성, 중성, EX]
+    private String memberGender; //성별 [남성, 여성, 중성, EX]
 
     public boolean searchIsEmpty() {
         return (this.memberName == "" || this.memberName == null) && this.memberGender == null;
