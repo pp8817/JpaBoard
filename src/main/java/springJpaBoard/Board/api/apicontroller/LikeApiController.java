2d3f3e82887256e85f99e87459ee043a34931c9e
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-01-16 15:03:20 +0900

    [refactor] 게시글 좋아요 메서드의 로그인 세션 처리 부분을 어노테이션으로 변경

--- src/main/java/springJpaBoard/Board/api/apicontroller/LikeApiController.java
+++ src/main/java/springJpaBoard/Board/api/apicontroller/LikeApiController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import springJpaBoard.Board.SessionConst;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import springJpaBoard.Board.domain.Board;
 import springJpaBoard.Board.domain.Member;
+import springJpaBoard.Board.domain.argumenresolver.Login;
 import springJpaBoard.Board.service.BoardService;
 import springJpaBoard.Board.service.LikeService;
 
@@ -19,7 +21,7 @@ public class LikeApiController {
     private final BoardService boardService;
 
     @GetMapping("/up/{boardId}")
-    public ResponseEntity addLike(@PathVariable Long boardId, @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember) {
+    public ResponseEntity addLike(@PathVariable Long boardId, @Login Member loginMember) {
 
         Board board = boardService.findOne(boardId);
         likeService.addLike(loginMember, board);
