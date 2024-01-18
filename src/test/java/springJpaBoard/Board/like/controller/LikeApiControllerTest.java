745b9b25a3ffe7e3bb421dee5489a2f5f8fd39f2
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-01-18 21:02:23 +0900

    [refactor] 게시글 좋아요 테스트 리팩토링

--- src/test/java/springJpaBoard/Board/like/controller/LikeApiControllerTest.java
+++ src/test/java/springJpaBoard/Board/like/controller/LikeApiControllerTest.java
@@ -1,8 +1,10 @@
 package springJpaBoard.Board.like.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,7 +13,6 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import springJpaBoard.Board.SessionConst;
 import springJpaBoard.Board.api.apicontroller.LikeApiController;
 import springJpaBoard.Board.domain.Board;
 import springJpaBoard.Board.domain.Member;
@@ -25,8 +26,7 @@ import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static springJpaBoard.Board.UtilsTemplate.getBoard;
-import static springJpaBoard.Board.UtilsTemplate.getMember;
+import static springJpaBoard.Board.UtilsTemplate.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(LikeApiController.class)
@@ -45,22 +45,29 @@ public class LikeApiControllerTest {
     @MockBean
     LikeRepository likeRepository;
 
-        protected MediaType contentType =
-                new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
+    protected MediaType contentType =
+            new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
+
+    @Mock
+    Member member;
+    @Mock
+    Board board;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        member = getMember();
+        board = getBoard();
+    }
 
     @Test
     @DisplayName("[POST] 게시글 좋아요 - 로그인 세션 유효")
     public void 게시글_좋아요_로그인_세션_유효() throws Exception {
         //given
 
-        Board board = getBoard();
-        Member member = getMember();
-
         given(boardService.findOne(any()))
                 .willReturn(board);
 
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute(SessionConst.LOGIN_MEMBER, member);
+        final MockHttpSession session = getSession(member);
 
         //when
         mockMvc.perform(get("/api/likes/up/{boardId}", 1L)
@@ -77,9 +84,7 @@ public class LikeApiControllerTest {
     @DisplayName("[POST] 게시글 좋아요 - 로그인 세션 유효 X")
     public void 게시글_좋아요_로그인_세션_유효_X() throws Exception {
         //given
-
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute(SessionConst.LOGIN_MEMBER, null);
+        final MockHttpSession session = getSession(null);
 
         //when
         ResultActions actions = mockMvc.perform(get("/api/likes/up/{boardId}", 1L)
