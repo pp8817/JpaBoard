cfa0e84083c711bfd9ca95e1aa850f7232e57cb4
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-01-20 18:18:05 +0900

    [refactor] 에러 코드 속성 수정

--- src/main/java/springJpaBoard/Board/global/Error/exception/ErrorCode.java
+++ src/main/java/springJpaBoard/Board/global/Error/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
 
     //Common
     INVALID_INPUT_VALUE(400, "C001", "잘못된 입력값입니다."),
-    METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405, "C002", "잘못된 입력값입니다."),
     ENTITY_NOT_FOUND(400, "C003", "존재하지 않는 엔티티입니다."),
     INTERNAL_SERVER_ERROR(500, "C004", "서버 오류"),
     INVALID_TYPE_VALUE(400, "C005", " 잘못된 타입입니다."),
@@ -23,7 +23,7 @@ public enum ErrorCode {
     //Member
     NICKNAME_DUPLICATION(409, "U001", "중복되는 닉네임입니다."),
     NOT_FOUND_USER(404, "U002", "존재하지 않는 회원입니다."),
-    INVALID_INPUT_ID_PASSWORD(400, "U003", "Id와 Password가 일치하지 않습니다."),;
+    INVALID_INPUT_ID_PASSWORD(400, "U003", "Id잘모 Password가 일치하지 않습니다."),;
 
     //Board
 
